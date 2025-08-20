4c5f82314da5d0c4dd858a5b15cc6b60a5a5cb4f
karanjeet-0422 <karanjeet0422@gmail.com>
2025-08-20 22:01:39 +0530

    Learning new things 20 aug 2025

---
+++ tut4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 14, b = 15;
+    float pi = 3.14;
+    char c = 'ud';
+    cout << "This is tutorial 4\nThe value of A is: " << a << "\nThe Value of B is: " << b;
+    cout << "\nThe value of pi is: " << pi;
+    cout << "\nThe Value of c is: " << c;
+    return 0;
+}
